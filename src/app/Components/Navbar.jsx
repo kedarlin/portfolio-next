5222703ef22b11c197fd44764f745d363007c41e
kedarlin <kedarkamaf@gmail.com>
2024-03-04 19:30:57 +0530

    added navbar

---
+++ src/app/Components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Link from "next/link";
+import NavLink from "./NavLink";
+
+const navLinks = [
+  {
+    title: "About",
+    path: "#about",
+  },
+  {
+    title: "Projects",
+    path: "#projects",
+  },
+  {
+    title: "Contact",
+    path: "#contact",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="flex flex-wrap item-center justify-between mx-auto p-8">
+        <Link href={"/"} className="text-5xl text-white font-semibold">
+          LOGO
+        </Link>
+        <div className="menu block md:w-auto" id="navbar">
+          <ul>
+            {navLinks.map((link, index) => {
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>;
+              console.log(index);
+            })}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import HeroSection from "./Components/HeroSection";
+import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg=[#121212]">
+      <Navbar />
       <div className="container mx-auto px-12 py-4">
         <HeroSection />
       </div>
