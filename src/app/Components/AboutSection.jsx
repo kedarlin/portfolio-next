17bf5b3e63d98eb7619058e6f3e1eab523e4b8eb
kedarlin <kedarkamaf@gmail.com>
2024-03-05 19:52:24 +0530

    edited navbar and added about-section

---
+++ src/app/Components/AboutSection.jsx
@@ -0,0 +1,102 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+    {
+        title:"Skills",
+        id:"skills",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>Node.Js</li>
+                <li>Express</li>
+                <li>React Js</li>
+                <li>Next Js</li>
+                <li>Cassandra</li>
+                <li>Problem Solving</li>
+                <li>Blender</li>
+            </ul>
+        )
+    },
+    {
+        title:"Education",
+        id:"education",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>St Joseph's College Of Engineering</li>
+                <li>Ramakrishna Mission Mat Hr Sec School</li>
+            </ul>
+        )
+    },
+    {
+        title:"Certifications",
+        id:"certifications",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>Node.Js IBM Coursera</li>
+                <li>Cyber Security Foundations - Google Coursera</li>
+            </ul>
+        )
+    }
+]
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition ] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+        setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:py-16">
+        <Image
+          src="/images/about-img.jpg"
+          width={500}
+          height={500}
+          alt="about-img"
+        />
+        <div className="mt-4 md:mt-0 text-left flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I'm a full stack web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with Javascript, ReactJs, Redux, Next Js, Node.Js, Express,
+            MySQL, and Blender. I am a quick learner and I am always looking to
+            expand my knowledge and skill set. I am a team player and I am
+            excited to work with others to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >{" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >{" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >{" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import HeroSection from "./Components/HeroSection";
 import Navbar from "./Components/Navbar";
+import AboutSection from "./Components/AboutSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg=[#121212]">
       <Navbar />
-      <div className="container mx-auto px-12 py-4">
+      <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
+        <AboutSection />
       </div>
     </main>
   );
